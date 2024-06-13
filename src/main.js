710d71ec8d391ddd75f6c0e089ef79e0146e2c08
Gabriel Ciloé <gabiciloe@gmail.com>
2024-06-13 20:24:49 -0300

    APP FocusTimer
    APP FocusTimer

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './toggle-mode.js'
+import * as FocusTimer from '../FocusTimer/index.js'
+/* (* as FocusTimer agrupou tudo que está sendo exportado e colocando no objeto FocusTimer)*/
+
+FocusTimer.start(0, 0)
