ebf529cda8a8f7cd123b7a8db92cc86386591ddf
Kevilly <vickevilly123@gmail.com>
2024-06-13 21:11:51 -0300

    change bg audio
    Falar com Deus moment

--- FocusTimer/sounds.js
+++ FocusTimer/sounds.js
@@ -2,6 +2,6 @@ export const buttonPressAudio = new Audio('./assets/button-press.wav');
 
 export const kichenTmer = new Audio('./assets/kichen-timer.mp3');
 
-export const bgAudio = new Audio('./assets/bg-audio.mp3');
+export const bgAudio = new Audio('./assets/bg-audio.m4a');
 
 bgAudio.loop = true
